5570cd0ff6ee13b0e652a0e96d5b7ffd6404d60d
Dylan King <dwkblue51@yahoo.com>
2021-07-22 15:56:14 -0500

    create projectpage and start on module

--- src/projectpage.js
+++ src/projectpage.js
@@ -1,2 +1,7 @@
 // user is directed to this page after clicking on a project
 // shows info about the project and its todos
+
+const projectPage = (() => {
+    const container = document.createElement('div');
+    
+})();
